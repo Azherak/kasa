59e4da0702e55e074fcf0f527ce91f6e767a9424
florian denis <136095872+Azherak@users.noreply.github.com>
2024-02-15 15:21:51 +0100

    ajout page error 404
    -ajout dans path
    -ajout link vers accueil

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/index.jsx';
 import Home from './components/Home/index.jsx';
 import Footer from './components/Footer/index.jsx';
 import About from './components/About/index.jsx';
+import Error from './components/Error/index.jsx';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error />} /> 
         </Routes>
         <Footer />
       </div>
