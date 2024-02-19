b5c4ccf684d5a255f10adbecfdcfb77aa188ea51
florian denis <136095872+Azherak@users.noreply.github.com>
2024-02-19 17:11:44 +0100

    debut fix logements
    -manque rating etoiles
    - a modifier equipements

--- src/components/Logements/index.jsx
+++ src/components/Logements/index.jsx
@@ -2,39 +2,38 @@ import { useParams } from 'react-router-dom';
 import InfoBox from "../About/InfoBox/index";
 import data from "../../data.json"; 
 import ImageCarousel from "./Carousel/index.jsx";
+import "./Logements.css";
 
 function Logements(){
     const {id} = useParams();
-    console.log(id);
     const logement = data.find(item => item.id === id);
     if (!logement) {
         return <div>Logement non trouvé</div>; 
     } 
-    console.log(id)
     return(
-        <div>
+        <div className='logements'>
             <div>
                 <ImageCarousel pictures={logement.pictures}/> 
             </div>
-            <div>
+            <div className='titre-hotes'>
                 <div>
                     <h2>{logement.title}</h2>
                     <p>{logement.location}</p>
                 </div>
-                <div>
-                    <h3>{logement.host.name}</h3>
+                <div className='hotes'>
+                    <p>{logement.host.name}</p>
                     <img src={logement.host.picture} alt="visage de l'hôte"></img>
                 </div>
             </div>
-            <div>
-                <div>
-                    <div>{logement.tags[0]}</div>
-                    <div>{logement.tags[1]}</div>
-                    <div>{logement.tags[2]}</div>
+            <div className='rating-conteneur'>
+                <div className='conteneur-box'>
+                    <div className='box-info'>{logement.tags[0]}</div>
+                    <div className='box-info'>{logement.tags[1]}</div>
+                    <div className='box-info'>{logement.tags[2]}</div>
                 </div>
                 <div>rating</div>
             </div>
-            <div>
+            <div className='description-equipements'>
                 <InfoBox word="Description" explanation={logement.description}/>
                 <InfoBox word="Equipements" explanation={logement.equipments}/>
             </div>
