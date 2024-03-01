7837cca5b171386498aaefe2516a5285bc65be3e
florian denis <136095872+Azherak@users.noreply.github.com>
2024-03-01 09:44:28 +0100

    media:. 375px pour home

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,7 +10,7 @@ function About(){
                 <img src={img} alt="montagnes et lac"></img>
             </div>
             <div>
-                <InfoBox word="Fiabilité" explanation="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformesaux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+                <InfoBox word="Fiabilité" explanation="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
                 <InfoBox word="Respect" explanation="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de la plateforme." />
                 <InfoBox word="Service" explanation="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de la plateforme." />
                 <InfoBox word="Sécurité" explanation="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
