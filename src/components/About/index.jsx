47e5c05780517affcb884c0219cb0d40791780ed
florian denis <136095872+Azherak@users.noreply.github.com>
2024-03-01 10:46:07 +0100

    fix footer

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,7 +5,7 @@ import InfoBox from "./InfoBox";
 
 function About(){
     return(
-        <>
+        <div className="about">
             <div className="about-visuel">
                 <img src={img} alt="montagnes et lac"></img>
             </div>
@@ -15,7 +15,7 @@ function About(){
                 <InfoBox word="Service" explanation="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de la plateforme." />
                 <InfoBox word="Sécurité" explanation="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
             </div>
-        </>
+        </div>
     )
 }
 
