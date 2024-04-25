aaaf24bfd495c50c54d4b6b2161773a4874d9f8f
florian denis <136095872+Azherak@users.noreply.github.com>
2024-04-25 17:26:14 +0200

    rajout des components scss

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/Logo-footer.png";
-import "../../style/index.css";
+
 
 function Footer() {
     return(
--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "../../assets/LOGO.png";
-import "../../style/index.css";
 import { Link } from "react-router-dom";
 
 function Header(){
